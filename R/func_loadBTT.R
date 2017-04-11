d9a5287639abacf07567ba08aaacd4b2640bcef9
willdrysdale <willdrysdale@googlemail.com>
2017-04-11 10:39:37 +0100

    Function update ussing Extdata
    Updated BTT load function to work with new extdata structure

--- R/func_loadBTT.R
+++ R/func_loadBTT.R
@@ -3,13 +3,14 @@
 #' Loads in BT tower data based on parameters in the btconfig.R file.
 #' Voncentration should be in the subdirectory "conc" and meterological data
 #' in the sub directory "met"
+#' Defaults to C:/Users/username/Documents/R/win-library/3.3/wsdmiscr/extdata/btconfig.R
 #' 
 #' @export
 
 
 load_BTT = function(){
   #load filepath
-  source("R/btconfig.R")
+  source(system.file("extdata/btconfig.R",package = "wsdmiscr"))
   #select conc file
   flist = dir(paste(btfilepath,"conc/",sep = ""))
   if (length(flist) > 1){
