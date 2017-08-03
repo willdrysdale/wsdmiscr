280018c380ffa8f0f218593cfe7c6332d4e256fa
willdrysdale <willdrysdale@googlemail.com>
2017-08-03 13:05:18 +0100

    process.aricraft2 added
    File structure has changed post flight C019, process.aircraft2 can
    handle the new structure

--- R/func_process_aircraft2.R
+++ R/func_process_aircraft2.R
@@ -1,6 +1,8 @@
-#' Process Aircraft Data
+#' Process Aircraft Data2
 #' 
 #' Reads in an AQD NOx aircraft file and runs aircraft_cal_flags, prodcuing quicklook and full outputs
+#' Used on Files after flight C019
+#' 
 #' 
 #' @param fn file to read
 #' @param dir_out output directory
@@ -15,15 +17,18 @@
 #' @export
 
 
-process.aircraft = function(fn,dir_out = "",flight_id = "XXXX"){
+process.aircraft2 = function(fn,dir_out = "",flight_id = "XXXX"){
   d = read.aircraft(fn)
   d_processed = aircraft_cal_flags(d)
-  d_quick = d_processed[,c(1,54,55,56)]
+  d_quick = d_processed[,c(1,65,66,67)]
   d_final = aircraft_clean_negatives(d_quick)
   write.csv(d_processed,paste(dir_out,flight_id,"_processed.csv",sep = ""),row.names = F,na = "")
   write.csv(d_quick,paste(dir_out,flight_id,"_quick.csv",sep = ""),row.names = F,na = "")
   write.csv(d_final,paste(dir_out,flight_id,"_final.csv",sep = ""),row.names = F,na = "")
   out = list(d_processed,d_quick,d_final)
-  names(out) = c(paste(flight_id,"_processed",sep = ""),paste(flight_id,"_quick",sep = ""))
+  names(out) = c(paste(flight_id,"_processed",sep = ""),
+                 paste(flight_id,"_quick",sep = ""),
+                 paste(flight_id,"_final",sep = "")
+                 )
   out
 }
