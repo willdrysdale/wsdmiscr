43e7d8cd5c454f3fd15a2cfcb795809ed9234ea3
willdrysdale <willdrysdale@googlemail.com>
2017-10-30 17:59:35 +0000

    Added eddy pro documentation

--- R/func_readeddypro.R
+++ R/func_readeddypro.R
@@ -1,3 +1,11 @@
+#' Read Eddy Pro
+#' 
+#' Reads Eddy pro outputs into R
+#' 
+#' @param filepath location of Eddy pro output (csv)
+#' 
+#' @export
+
 read.eddypro = function(filepath){
   headers = read.csv(filepath,skip = 1,nrow  = 1)
   headers = names(headers)
